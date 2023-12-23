5ceba09eaaf6f77550560da41dda71e84ee8a8a8
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-23 21:52:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+        // Get the body element
+        var bodyElement = document.body;
+
+        // Check if the body is empty (has no child nodes)
+        if (bodyElement.childNodes.length === 0) {
+            // Create a new text node with the message
+            var textNode = document.createTextNode('DOM load success');
+
+            // Append the text node to the body
+            bodyElement.appendChild(textNode);
+        }
+    });
